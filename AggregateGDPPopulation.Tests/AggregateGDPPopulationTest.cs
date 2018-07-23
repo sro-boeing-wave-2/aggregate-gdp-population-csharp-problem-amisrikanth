08e2d18896978e1465a23cad3c98314307726be6
Srikanth Banagadi <amisrikanth@gmail.com>
2018-07-23 17:27:43 +0530

    completed the assignment with Sync

--- AggregateGDPPopulation.Tests/AggregateGDPPopulationTest.cs
+++ AggregateGDPPopulation.Tests/AggregateGDPPopulationTest.cs
@@ -1,14 +1,24 @@
+using Newtonsoft.Json.Linq;
 using System;
+using System.IO;
 using Xunit;
+using AggregateGDPPopulation;
+
+
 
 namespace AggregateGDPPopulation.Tests
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public static void Test1()
         {
-
+            Class1.Method();
+            StreamReader readJSON = new StreamReader(@"../../../../Output/output.json");
+            string inputData = JObject.Parse(readJSON.ReadToEnd()).ToString();
+            StreamReader readExpectedJSON = new StreamReader(@"../../../expected-output.json");
+            string outputData = JObject.Parse(readExpectedJSON.ReadToEnd()).ToString();
+            Assert.Equal(outputData, inputData);
         }
     }
 }
--- AggregateGDPPopulation/AggregateGDPPopulation.cs
+++ AggregateGDPPopulation/AggregateGDPPopulation.cs
@@ -1,8 +1,76 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace AggregateGDPPopulation
 {
     public class Class1
     {
+        class ContinentInfo
+        {
+            public float GDP { get; set; }
+            public float POPULATION { get; set; }
+        }
+        class RequiredInfo
+        {
+            public string Country { get; set; }
+            public string Population { get; set; }
+            public string GDP { get; set; }
+        }
+
+        public static void Method()
+        {
+            string filePathCSV = @"../../../../AggregateGDPPopulation/data/datafile.csv";
+            List<string> contents = File.ReadAllLines(filePathCSV).ToList();
+            string[] header = contents[0].Split(',');
+            for(int i=0;i<header.Length;i++)
+            {
+                header[i] = header[i].Trim('\"');
+            }
+            int indexOfPopulation = Array.IndexOf(header, "Population (Millions) - 2012");
+            int indexOfGDP = Array.IndexOf(header, "GDP Billions (US Dollar) - 2012");
+            int indexOfCountry = Array.IndexOf(header, "Country Name");
+            List<RequiredInfo> output = new List<RequiredInfo>();
+            for (int i = 1; i < contents.Count()-1; i++)
+            {
+                string[] info = contents[i].Split(',');
+
+                RequiredInfo inputOfInfo = new RequiredInfo
+                {
+                    Country = info[indexOfCountry].Trim('\"'),
+                    Population = info[indexOfPopulation].Trim('\"'),
+                    GDP = info[indexOfGDP].Trim('\"')
+                };
+                output.Add(inputOfInfo);
+            }
+            StreamReader readJSON = new StreamReader(@"../../../../AggregateGDPPopulation/data/country-continent-map.json");
+            
+            JObject data = JObject.Parse(readJSON.ReadToEnd());
+            Dictionary<string,string> convertedData = JsonConvert.DeserializeObject< Dictionary<string, string>>(data.ToString());
+
+            Dictionary<string, ContinentInfo> continentGDPPopulation = new Dictionary<string, ContinentInfo>();
+            foreach(var item in output)
+            {
+                if (continentGDPPopulation.ContainsKey(convertedData[item.Country]))
+                {
+                    continentGDPPopulation[convertedData[item.Country]].GDP += float.Parse(item.GDP);
+                    continentGDPPopulation[convertedData[item.Country]].POPULATION += float.Parse(item.Population); 
+                }
+                else
+                {
+                    ContinentInfo newContinent = new ContinentInfo { GDP = float.Parse(item.GDP), POPULATION = float.Parse(item.Population) };
+                    continentGDPPopulation.Add(convertedData[item.Country], newContinent);
+                }
+                
+            }
+
+            var outputText = JsonConvert.SerializeObject(continentGDPPopulation);
+            File.WriteAllText("../../../../Output/output.json", outputText);
+
+        }
+
     }
 }
